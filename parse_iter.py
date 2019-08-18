1a1c8797faeb143fddd07a4cba367b72e24f2a2c
varun.bhtt21 <varun.bhatt@students.iiit.ac.in>
2019-08-18 13:25:24 +0530

    Parsing all most done

--- parse_iter.py
+++ parse_iter.py
@@ -1,74 +1,53 @@
 import xml.etree.cElementTree as ET
-file_path = "wiki.xml"
-context = ET.iterparse(file_path, events=("start", "end"))
 
 
 def processing(word):
-
     try:
         word = word.split('}')[1]
     except:
         pass
-
     return word
 
 
-count = 0
-# turn it into an iterator
-context = iter(context)
-on_members_tag = False
 
-for event, elem in context:
-    tag = elem.tag
-    value = elem.text
-    
-    if value :
-        value = value.encode('utf-8').strip()
+def main():
+    count = 0
+    for event, elem in context:
+        tag = elem.tag
 
-    if event == 'start' :
-        pass
+        if event == 'end':
+            given_tag = processing(str(tag))
 
+            if given_tag == 'page':
+                count += 1
+                f = open("Data/DOC "+str(count), 'w')
 
+                # Getting Information of Title and Text
+                for titl in elem:
+                    page_tag = processing(str(titl.tag))
 
-
-    if event == 'end':
-
-        # FOR PAGES
-        page = str(tag)
-        page = processing(page)
-        if page == 'page':
-            count += 1
-            # f = open("Data/DOC "+str(count), 'w')
-
-            # FOR TITLES
-            for tit in elem:
-
-                title = tit.tag
-                value = tit.text
-                
-               
-                title = str(title)
-                title = processing(title)
-
-                if title == 'title':
-                    print("title : ",tit.text)
-                    break
-                    if type(tit.text) is not None:
-                        print("yes")
-                        # f.write("Title : " + tit.text.encode('utf8') + "\n")
+                    if page_tag == 'title':
+                        # print("title : ", titl.text)
                         
-                              
-                # FOR TEXT
-                if title == 'revision':
-                    for tex in tit:
-                        if processing(str(tex)) == 'text':
+                        if type(titl.text) is not None:
+                            f.write("Title : " + titl.text.encode('utf8') + "\n")
 
-                            # Extract_Infobox(str(tex.text.encode('utf8')))
-                            print("text : ", tex.text)
+                    if page_tag == 'revision':
+                        for tex in titl:
+                            if processing(str(tex.tag)) == 'text':
+                                # print("text : ", tex.text)
 
-                            if tex.text is not None:
-                                print("No")
-                                # f.write("Text : " + tex.text.encode('utf8') + "\n")
-                                                                               
-            # f.close()
-    elem.clear()
+                                if tex.text is not None:
+                                    f.write("Text : " + tex.text.encode('utf8') + "\n")
+                             
+                elem.clear()
+
+
+
+
+
+if __name__== "__main__":
+    file_path = "wiki.xml"
+    context = ET.iterparse(file_path)
+    main()
+    print("Done")

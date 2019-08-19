921ddc29c5cec798b4a540b68c5a9864dd4098cb
varun.bhtt21 <varun.bhatt@students.iiit.ac.in>
2019-08-19 20:31:14 +0530

    Infobox, Links, Category values Retrieved

--- parse_iter.py
+++ parse_iter.py
@@ -1,6 +1,10 @@
 import xml.etree.cElementTree as ET
+import mwparserfromhell
 
 
+
+#**************************************************************************
+# String Processing Function [Use to clear the namespaces]
 def processing(word):
     try:
         word = word.split('}')[1]
@@ -10,6 +14,55 @@ def processing(word):
 
 
 
+#**************************************************************************
+# Get Infobox Values
+def Infobox_Extraction(page):
+    """Parse out the first Infobox for the page as a dict."""
+    templates = mwparserfromhell.parse(page).filter_templates()
+    # filter_templates() : Make the list out of the text
+    # strip()            : Remove left and right spaces
+
+    infobox = {}
+    for template in templates:
+        if template.name.strip_code().startswith('Infobox'):
+            infobox = {
+                str(p.name).strip(): p.value.strip_code().strip()
+                for p in template.params if p.value.strip_code().strip()
+            }
+    return infobox 
+
+
+
+#**************************************************************************
+# Get References
+def References(page):
+    templates = mwparserfromhell.parse(page).filter_external_links()
+    # filter_templates() : Make the list out of the text
+    # strip() : remove left and right spaces
+    print(templates)
+
+
+
+#**************************************************************************
+# Get Category Values
+def get_Category(page):
+
+    for i in page.splitlines():
+        if "Category" in i:
+            Category = i.split(":")
+
+            if len(Category)>1:
+                print(Category[1].split("]]")[0])
+
+
+#**************************************************************************
+# Get Body Tag Value
+def body_tag(page):
+    
+
+
+#**************************************************************************
+# Main Function
 def main():
     count = 0
     for event, elem in context:
@@ -18,19 +71,25 @@ def main():
         if event == 'end':
             given_tag = processing(str(tag))
 
+        #********************************************************************
+        # Check for Page
+
             if given_tag == 'page':
                 count += 1
                 f = open("Data/DOC "+str(count), 'w')
 
-                # Getting Information of Title and Text
+        #*********************************************************************
+        # Getting Information of Title and Text
+
                 for titl in elem:
                     page_tag = processing(str(titl.tag))
 
                     if page_tag == 'title':
+                        pass
                         # print("title : ", titl.text)
                         
-                        if type(titl.text) is not None:
-                            f.write("Title : " + titl.text.encode('utf8') + "\n")
+                        # if titl.text is not None:
+                        #     f.write("Title : " + titl.text.encode('utf8') + "\n")
 
                     if page_tag == 'revision':
                         for tex in titl:
@@ -38,7 +97,12 @@ def main():
                                 # print("text : ", tex.text)
 
                                 if tex.text is not None:
-                                    f.write("Text : " + tex.text.encode('utf8') + "\n")
+                                    # Infobox_Extraction(tex.text)
+                                    # References(tex.text)
+                                    get_Category(tex.text)
+                                    
+                                    
+                                    # f.write("Text : " + tex.text.encode('utf8') + "\n")
                              
                 elem.clear()
 
